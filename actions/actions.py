1f1d5b63458986b5f0ca2c7655b7b8c72cd24901
atulrajdutta <adutta112358@etechtexas.com>
2023-04-14 13:45:35 +0530

    initial commit

--- actions/actions.py
+++ actions/actions.py
@@ -19,13 +19,13 @@ mongo_uri= "mongodb://localhost:27017/rasaUser"
 
 #         return []
 
-def storedata(sender_id,name,email):
-    myclient = pymongo.MongoClient(mongo_uri)
-    mydb = myclient["rasaUser"]
-    mycol = mydb["userData"]
-    myquery = {"sender_id":sender_id}
-    newvalue = {"$set" : {"name":name},"$set" : {"email":email}}
-    mycol.update_one(myquery,newvalue,upsert=True)
+# def storedata(sender_id,name,email):
+#     myclient = pymongo.MongoClient(mongo_uri)
+#     mydb = myclient["rasaUser"]
+#     mycol = mydb["userData"]
+#     myquery = {"sender_id":sender_id}
+#     newvalue = {"$set" : {"name":name},"$set" : {"email":email}}
+#     mycol.update_one(myquery,newvalue,upsert=True)
 
 class SubmitUserDetail(Action):
 
@@ -39,7 +39,7 @@ class SubmitUserDetail(Action):
         name= tracker.get_slot("name")
         email= tracker.get_slot("email")
 
-        storedata(sender_id,name,email)
+        # storedata(sender_id,name,email)
         dispatcher.utter_message(text="We are connecting you to our agent")
 
         return []
